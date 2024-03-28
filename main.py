d24d576d67a400001cc3927f5be17160fab9f2bb
PavelStarun <starunpavelpython@gmail.com>
2024-03-28 21:25:29 +0300

    add code basis
    основа

---
+++ main.py
@@ -0,0 +1,8 @@
+class Task():
+    def __init__(self, description, deadline, status):
+        self.description = description
+        self.deadline = deadline
+        self.status = status
+
+    def task_description(self):
+       
